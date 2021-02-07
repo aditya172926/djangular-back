a23eb35aaa8dce6cf014c00fb3922063ba840b69
Aditya <aditya26sg@gmail.com>
2021-02-07 11:54:13 +0530

    Complete Logout successful

--- myproject/api/views.py
+++ myproject/api/views.py
@@ -4,7 +4,9 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from myproject.api.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
@@ -23,6 +25,12 @@ class UserViewSet(viewsets.ModelViewSet):
 @csrf_exempt
 def userLogout(request):
     dictionary = json.loads(request.body)
-    tokenKey = dictionary['key']
-    Token.objects.get(key = tokenKey).delete()
+    token_user_id = dictionary['key']
+    Token.objects.get(user_id = token_user_id).delete()
     return HttpResponse('Done')
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        rresponse = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key = rresponse.data['token'])
+        return Response({'token': token.key, 'id': token.user_id})
